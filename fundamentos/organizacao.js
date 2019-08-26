58acd548c5d9c4f2ef40709071bdbab74646260e
Lucas Oliveira Silva <33430106+lucasolsi@users.noreply.github.com>
2019-08-26 17:49:56 -0300

    03

--- fundamentos/organizacao.js
+++ fundamentos/organizacao.js
@@ -1,24 +1,24 @@
-let a = 2;
+/* let a = 2;
 
 //Funções
 
-function testaSoma(a, b){
-    console.log(a+b);
+function testaSoma(a, b) {
+    console.log(a + b);
 }
 
-function outraSoma(a, b = 0){
-    return a+b;
+function outraSoma(a, b = 0) {
+    return a + b;
 }
 
 testaSoma(4, 3);
 console.log(outraSoma(10, 19));
 
 const novaSoma = (a, b) => {
-    return a+b;
+    return a + b;
 }
-console.log(novaSoma(10,2));
+console.log(novaSoma(10, 2));
 
-const maisSoma = (a, b) => a+b;
+const maisSoma = (a, b) => a + b;
 console.log(maisSoma(9, 18));
 
 //Array
@@ -33,3 +33,34 @@ console.log(valores.length);
 valores.pop();
 
 console.log(valores);
+
+//Boolean
+let isAtivo = false;
+console.log(isAtivo);
+
+let isActive = true;
+console.log(isActive);
+
+isActive = 1;
+console.log(!isActive);
+
+// Object
+
+const prod1 = {}
+prod1.nome = "Teste"
+prod1.valor = '999.00'
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Produto2',
+    teste: 'Teste1',
+    valor: 80.99
+}
+
+console.log(prod2)
+ */
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log('i = ', i);
