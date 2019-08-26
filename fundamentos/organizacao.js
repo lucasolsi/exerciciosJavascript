a2eb1134d720a9bf4228fd0bf09d100dd8f6439e
Lucas Oliveira Silva <33430106+lucasolsi@users.noreply.github.com>
2019-08-26 11:01:28 -0300

    02

--- fundamentos/organizacao.js
+++ fundamentos/organizacao.js
@@ -1,5 +1,7 @@
 let a = 2;
 
+//Funções
+
 function testaSoma(a, b){
     console.log(a+b);
 }
@@ -18,3 +20,16 @@ console.log(novaSoma(10,2));
 
 const maisSoma = (a, b) => a+b;
 console.log(maisSoma(9, 18));
+
+//Array
+const valores = [3.14, 5, 2];
+console.log(valores[0], valores[2]);
+
+valores[10] = 50;
+console.log(valores);
+
+console.log(valores.length);
+
+valores.pop();
+
+console.log(valores);
