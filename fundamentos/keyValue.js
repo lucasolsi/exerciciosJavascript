66261dba0e471a26d1828508b5ecd9c20f2a2621
Lucas Oliveira Silva <33430106+lucasolsi@users.noreply.github.com>
2019-08-29 10:22:26 -0300

    06

--- fundamentos/keyValue.js
+++ fundamentos/keyValue.js
@@ -1,1 +1,19 @@
-const teste = 'teste';
+const teste = 'This is a test';
+
+function exec() {
+    const teste = 'Another test';
+    return teste;
+}
+
+const client = {
+    name: 'Cliente',
+    age: 30,
+    address: {
+        street: 'Rua das Casas, 200',
+        zipCode: 02105
+    }
+}
+
+console.log(teste);
+console.log(exec());
+console.log(client);
