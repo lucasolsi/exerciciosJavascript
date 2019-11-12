b46a0d43e65030d104e9092ccd611d630ade4e19
Lucas Oliveira Silva <lucasolsi@gmail.com>
2019-11-12 20:02:54 -0300

    Update

---
+++ html/form/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/users', (req, resp) => {
+    console.log(req.body);
+    resp.send('<h1>Done!</h1>');
+});
+
+app.post('/users/:id', (req, resp) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    resp.send('<h1>Done updating!</h1>');
+});
+
+app.listen(3003);
+
